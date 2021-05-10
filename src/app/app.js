2332267016c1ba1f48018c43e5a928f5acb1b500
Yevaeva <yeva.terteryan@gmail.com>
2021-05-10 14:05:30 +0400

    cors

--- src/app/app.js
+++ src/app/app.js
@@ -58,8 +58,7 @@ app.use((err, req, res, next) => {
 // Add headers
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_HOST || '*');
-
+  res.setHeader('Access-Control-Allow-Origin',  '*');
   // Request methods you wish to allow 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
